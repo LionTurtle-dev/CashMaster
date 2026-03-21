1f671417fb3ab68848eea5d44a3c30309bff191b
LionTurtle-dev <patakitasbalint@gmail.com>
2026-03-21 15:00:52 +0100

    [HUN]: Programhibák kijavítva.
    [ENG]: It has been debugged.

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ const sum = document.getElementById("sum");          //Össz-ár címkéje
 const count = document.getElementById("count");      //Kiszámoló gomb
 const buy = document.getElementById("buy");          //Vásárlás véglegesítése
 const save = document.getElementById("save");        //Adatok mentése
-let tételek = '';                                    //A Lista elemeit tartalmazó HTML string
 let IP;
-let minden;
 let vetel = {cuccok: [], összeg: 0};                                           //Egy vásárlás részletei
 
 //Árukészlet két adattáblában
@@ -13,13 +11,12 @@ let készlet;
 
 //IP cím beszerzése
 function GetIP() {
-    IP = prompt('Add meg az IP címet!')
-    //Requester.style.display = "block";
+    IP = prompt('Add meg az IP címet!');
 }
 
 //Adatok letöltése a szerverről
 function GetData() {
-    fetch(`http://${IP}:5555/keszlet_2.json`)
+    fetch(`http://${IP}:5555/keszlet.json`)
         .then(response => response.json())
         .then(data => {
             készlet = data;
@@ -30,38 +27,63 @@ function GetData() {
         })
 }
 
+function soldOut() {
+    for ( let cucc of készlet.megmaradt ) {
+        if (cucc[2] > 0 ) {
+            return false;
+        } else {
+            continue;
+        }
+    }
+    return true;
+}
+
 //Adatok mentése a szerverre
 function GiveData() {
-    fetch(`http://${IP}:5555/keszlet_2.json`, {
+    fetch(`http://${IP}:5555/keszlet.json`, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(v)
-    });
+        body: JSON.stringify(készlet)
+    })
+        .then(() => {
+            if (soldOut()) {
+                alert('Az adatok sikeresen mentve.');
+                alert("\tHURRÁ!\nMindent eladtál!!! ^^");
+            } else {
+                alert('Az adatok sikeresen mentve.');
+            }
+        })
 }
 
 //---------------------------------------------------------------
 
 //  Áruk kezelési listájának betöltése
-function load(v) {
+function load() {
+    let tételek = "";
     for (let i = 0; i < készlet.árlista.length; i++) {
         if (készlet.megmaradt[i][2] > 0) {
-            tételek += '<li><label for="ck_' + (i + 1) + '">' + készlet.árlista[i][0] + ' ('+ készlet.árlista[i][1] +' Ft/db, ' + készlet.megmaradt[i][2] + ' db maradt)</label><input class="measure" type="number" id="ck_' + (i + 1) + '" value="0"><br><label class="errs" id="E-'+ (i + 1) +'">Nem adhatsz meg nagyobb összeget, mint '+ készlet.megmaradt[i][2] +', sem kissebbet, mint 0.</label></li>'
+            tételek += '<li><label for="ck_' + (i + 1) + '">' + készlet.árlista[i][0] + ' ('+ készlet.árlista[i][1] +' Ft/db, ' + készlet.megmaradt[i][2] + ' db maradt)</label><input class="measure" type="number" id="ck_' + (i + 1) + '" value="0"><br><label class="errs" id="E-'+ (i + 1) +'">Nem adhatsz meg nagyobb összeget, mint '+ készlet.megmaradt[i][2] +', sem kissebbet, mint 0.</label></li>';
         }
     }
-    cuccok.innerHTML = tételek
-    sum.innerText = " 0"
+    cuccok.innerHTML = tételek;
+    sum.innerText = " 0 Ft";
+
 }
 
 //  Összekészített áruk értékének összegzése
 function add_prices() {
     vetel = {cuccok: [], összeg: 0};
-    minden = 0;                                              //Végösszeg értéke 
+    let minden = 0;                                              //Végösszeg értéke 
     let nonull = true;
-    let parts = []
+    let parts = [];
+    let reszosszeg;
     for (let i = 1; i <= készlet.megmaradt.length; i++) {
         let inp = document.getElementById("ck_" + i.toString()); //Össz érték termék szerint
         let Err = document.getElementById("E-"+ (i));
-        let tmp = []
+        let tmp = [];
+        if (inp === null) {
+            continue;
+        }
         if (készlet.megmaradt[i - 1][2] >= inp.valueAsNumber && inp.valueAsNumber >= 0) {
             reszosszeg = inp.valueAsNumber * készlet.megmaradt[i - 1][1];
             minden += reszosszeg;
@@ -69,7 +91,7 @@ function add_prices() {
             if (inp.valueAsNumber > 0) {
                 parts.push(false);
                 tmp = [készlet.megmaradt[i - 1][0], készlet.megmaradt[i - 1][1], inp.valueAsNumber];
-                vetel.cuccok.push(tmp)
+                vetel.cuccok.push(tmp);
                 vetel.összeg += reszosszeg; 
             }
         } else {
@@ -80,23 +102,41 @@ function add_prices() {
     for (let part of parts) {
         if (part == false) {
             nonull = false;
+            break;
         }
     }
 
-    sum.innerText = minden.toString() + ' Ft'                   //Össz-ár címkét átírja a program a végösszeg értékére forintban
+    sum.innerText = minden.toString() + ' Ft';                  //Össz-ár címkét átírja a program a végösszeg értékére forintban
     if (nonull != true) {
         buy.style.display = "block";
     } else {
         buy.style.display = "none";
+        save.style.display = "none";
     }
 }
 
 /*//Összekészített árúk vásárlásának véglegesítése
 function buy-prod() {} */
 
+function keres() {
+    for (let cucc of vetel.cuccok) {
+        for (let i = 0; i < készlet.megmaradt.length; i++) {
+            if (cucc[0] === készlet.megmaradt[i][0]) {
+                készlet.megmaradt[i][2] -= cucc[2];
+            }
+        }
+    }
+}
+
+
 function buy_products() {
     if (vetel.cuccok.length > 0) {
         készlet.tranzakciók.push(vetel);
+        készlet.bevétel += vetel.összeg;
+        keres();
+        load();
+        save.style.display = "block";
+        buy.style.display = "none";
     }
 }
 
@@ -104,4 +144,5 @@ GetIP();
 GetData();
 
 count.addEventListener("click", () => { add_prices() });
-save.addEventListener('click', () => { saveProd() });
+buy.addEventListener("click", () => { buy_products() });
+save.addEventListener("click", () => { GiveData() });
